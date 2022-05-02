ba8e74b3583c03121eb3a8e5fc5134c8757c626f
Leon Antoshyn <Doctorsprait@gmail.com>
2022-05-02 14:05:42 +0300

    carousel added

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false, // показать стрелки
+        dots: true, // не показывать точки
+        slidesToShow: 1, // показывать по 3 слайда
+        infinite: true
+    });
+});
