2197cbf0bc84c8a4ac3244cce4e625ab721d2146
Leon Antoshyn <Doctorsprait@gmail.com>
2022-05-02 21:34:39 +0300

    button and modal window added

---
+++ mail.php
@@ -0,0 +1,44 @@
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
+
+require_once ('phpmailer/src/SMTP.php');
+require_once ('phpmailer/src/PHPMailer.php');
+
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$surname = $_POST['user_surname'];
+$email = $_POST['user_mail'];
+$phone = $_POST['user_phone'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'wanderers-extreme.pl';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'admin@wanderers-extreme.pl'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '6.IM&Q(=DEWA'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('admin@wanderers-extreme.pl'); // от кого будет уходить письмо?
+$mail->addAddress('sashavityaz1993@gmail.com');     // Кому будет уходить письмо     // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта';
+$mail->Body    = '' .$name .$surname .'<br>телефон'.$phone. '<br>имейл: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+   echo "<script>
+   alert('dziękuję bardzo!'); 
+   window.history.go(-1);
+   </script>";
+}
+
+?>    
